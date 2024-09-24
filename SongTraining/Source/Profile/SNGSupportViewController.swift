e5f327a695214197d58a52642b726cedecca1a6e
William Rena <wilyamx@gmail.com>
2024-09-24 16:22:55 +0800

    Update renamed package

--- SongTraining/Source/Profile/SNGSupportViewController.swift
+++ SongTraining/Source/Profile/SNGSupportViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import WSRComponents
+import WSRComponents_UIKit
 
 class SNGSupportViewController: SNGViewController, WSRStoryboarded {
 
--- SongTraining/Source/SignIn/SNGSignInViewController.swift
+++ SongTraining/Source/SignIn/SNGSignInViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import WSRComponents
+import WSRComponents_UIKit
 import WSRUtils
 
 class SNGSignInViewController: SNGViewController, WSRStoryboarded {
